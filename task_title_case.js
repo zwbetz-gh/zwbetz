d73f34dcd48158b6c4421b9a613aeb65dd264245
zwbetz-gh <zwbetz@gmail.com>
2021-03-11 23:57:52 -0600

    list of words not to title case

--- task_title_case.js
+++ task_title_case.js
@@ -35,20 +35,32 @@ const main = () => {
     const regex = /---\ntitle: ".*"/;
     const results = regex.exec(oldContent);
 
-    if (results && results[0]) {
-      const splitVal = '---\ntitle: ';
-      const oldTitleWithQuotes = results[0].split(splitVal)[1];
-      const beginQuote = /^"/;
-      const endQuote = /"$/;
-      const oldTitle = oldTitleWithQuotes
-        .replace(beginQuote, '')
-        .replace(endQuote, '');
-      const newTitle = oldTitle.toTitleCase();
-      console.log(newTitle);
-      const replaceVal = `---\ntitle: "${newTitle}"`;
-      const newContent = oldContent.replace(regex, replaceVal);
-      fs.writeFileSync(blogPostPath, newContent, {encoding: 'utf-8'});
+    if (!results || !results[0]) {
+      return;
     }
+
+    const splitVal = '---\ntitle: ';
+    const oldTitleWithQuotes = results[0].split(splitVal)[1];
+    const beginQuote = /^"/;
+    const endQuote = /"$/;
+    const oldTitle = oldTitleWithQuotes
+      .replace(beginQuote, '')
+      .replace(endQuote, '');
+
+    const newTitleRaw = oldTitle.toTitleCase();
+    const newTitle = newTitleRaw
+      .replace('Pg_dump', 'pg_dump')
+      .replace('E2e', 'e2e')
+      .replace('Localhost', 'localhost')
+      .replace('Psql', 'psql')
+      .replace('Tmux', 'tmux')
+      .replace('.Env', '.env');
+    console.log(oldTitle);
+    console.log(newTitle);
+
+    const replaceVal = `---\ntitle: "${newTitle}"`;
+    const newContent = oldContent.replace(regex, replaceVal);
+    fs.writeFileSync(blogPostPath, newContent, {encoding: 'utf-8'});
   });
 };
 
